9cc9282ddcda6ce04b40e249bbd07d1a5be8f8bf
rifatx <rifat.aricanli@gmail.com>
2017-10-11 15:20:24 +0300

    fix namespace name typo

--- samples/Serilog.Sinks.InfluxDB.ConsoleTester/Program.cs
+++ samples/Serilog.Sinks.InfluxDB.ConsoleTester/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SErilog.Sinks.InfluxDB.ConsoleTester
+namespace Serilog.Sinks.InfluxDB.ConsoleTester
 {
     class Program
     {
